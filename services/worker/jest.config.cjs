1b8f63c51f82c67046a6382425cc0622375b877a
TusharMicro135 <tushar.empiricinfotech@expert.micro1.ai>
2026-07-07 12:16:21 +0530

    Align TypeScript workspace validation

--- services/worker/jest.config.cjs
+++ services/worker/jest.config.cjs
@@ -1,1 +1,13 @@
-module.exports={displayName:'worker',testEnvironment:'node',preset:'ts-jest',testMatch:['<rootDir>/test/**/*.test.ts'],transform:{'^.+\\.tsx?$':['ts-jest',{tsconfig:{module:'CommonJS'}}]},coverageThreshold:{global:{branches:48,functions:48,lines:48,statements:48}}};
+module.exports = {
+  displayName: 'worker',
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+  testMatch: ['<rootDir>/test/**/*.test.ts'],
+  transform: { '^.+\\.tsx?$': ['ts-jest', { tsconfig: { module: 'CommonJS' } }] },
+  moduleNameMapper: {
+    '^@portal/shared-types$': '<rootDir>/../../packages/shared-types/src/index.ts',
+    '^@portal/shared-utils$': '<rootDir>/../../packages/shared-utils/src/index.ts',
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  coverageThreshold: { global: { branches: 48, functions: 48, lines: 48, statements: 48 } }
+};
